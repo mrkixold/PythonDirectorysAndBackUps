37e607eb2da85adf88d38b8de00cee1a2fc20699
Vicente <vicenteor101@gmail.com>
2022-07-25 14:56:31 +0200

    Adds File Property, getFileType method and RenameAllFiles Method

--- directory.py
+++ directory.py
@@ -3,6 +3,16 @@ import os
 class Directory: 
     def __init__(self, path):
         self.path = path
-    
-    files = os.listdir(path)
+        self.files = os.listdir(path)
+    def GetFileType(self, file):
+        filetype = file.split('.')[-1]
+        return filetype
+
+    def RenameAllFiles(self, newname):
+        counter = 0
+        for file in self.files:
+            filetype = self.GetFileType(file)
+            os.rename(self.path + '/' + file, self.path + '/' + newname + "_" + str(counter) + '.' + filetype)
+            print("Renaming " + file + " to " + newname + "_" + str(counter) + "." + filetype)
+            counter+=1
     
